c894a5f2ebdc09d6b8b880f42957861a4357f37f
helenaukraintseva <144246581+helenaukraintseva@users.noreply.github.com>
2024-03-28 22:35:17 +0300

    start
    start

---
+++ WeatherApp/WeatherApp/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace WeatherApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void ToMainWeatherPage()
+        {
+            await Navigation.PushModalAsync(new MainWeatherPage(cityEntry.Text));
+        }
+        private void findCityBtn_Clicked(object sender, EventArgs e)
+        {
+            ToMainWeatherPage();
+        }
+    }
+}
---
+++ WeatherApp/WeatherApp/MainWeatherPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace WeatherApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainWeatherPage : ContentPage
+    {
+        string city;
+        public MainWeatherPage(string city)
+        {
+            InitializeComponent();
+            this.city = city;
+        }
+        public MainWeatherPage()
+        {
+            InitializeComponent();
+            city = "Москва";
+        }
+    }
+}
